092eeb8b701c10036e83e530287c4ae725ff04d7
Lior <Shilon199707@gmail.com>
2023-08-19 16:38:15 +0200

    new

--- test/test.spec.js
+++ test/test.spec.js
@@ -8,8 +8,8 @@ describe("Arrays", () => {
         });
     })
 
-    describe("#sort", () => {
-        it("should sort an array", () => {
+    describe("#sort2", () => {
+        it("should sort an array2", () => {
             const arr = [2, 1, 3, 4, 5, 6, 7, 8, 9];
             expect(arr.sort()).to.be.eql([1, 2, 3, 4, 5, 6, 7, 8, 9]);
         });
