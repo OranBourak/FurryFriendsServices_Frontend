5835687e659b88d01e8f53673a97784b15bb2811
oran bourak <oranbourak@gmail.com>
2023-08-21 13:09:11 +0300

    Test added to check Circleci

--- test/test.spec.js
+++ test/test.spec.js
@@ -14,4 +14,11 @@ describe("Arrays", () => {
             expect(arr.sort()).to.be.eql([1, 2, 3, 4, 5, 6, 7, 8, 9]);
         });
     })
+
+    describe("#sort3", () => {
+        it("should sort an array3", () => {
+            const arr = [2, 1, 3, 4, 5, 6, 7, 8, 9];
+            expect(arr.sort()).to.be.eql([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        });
+    })
 });
