9363487ceb72cc68bc5c507dfa383872574adbb1
VladFearo <69718459+VladFearo@users.noreply.github.com>
2023-08-21 19:41:58 +0300

    react refactory

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TestNavbar from './components/TestNavbar.jsx';
+
+function App() {
+  return (
+    <>
+      <TestNavbar />
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
