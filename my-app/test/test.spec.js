30e560e592e35d701fb162a6dc00359c71e6d7df
VladFearo <69718459+VladFearo@users.noreply.github.com>
2023-08-22 22:29:34 +0300

    2nd try

--- my-app/test/test.spec.js
+++ my-app/test/test.spec.js
@@ -1,4 +1,4 @@
-import { expect } from "chai";
+import {expect} from "chai";
 
 describe("Arrays", () => {
     describe("#sort", () => {
@@ -6,13 +6,12 @@ describe("Arrays", () => {
             const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
             expect(arr.sort()).to.be.eql([1, 2, 3, 4, 5, 6, 7, 8, 9]);
         });
-    })
+    });
 
     describe("#sort2", () => {
         it("should sort an array2", () => {
             const arr = [2, 1, 3, 4, 5, 6, 7, 8, 9];
             expect(arr.sort()).to.be.eql([1, 2, 3, 4, 5, 6, 7, 8, 9]);
         });
-    })
-
+    });
 });
