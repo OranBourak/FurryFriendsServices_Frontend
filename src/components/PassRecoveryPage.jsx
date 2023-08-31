dfbe945908258ad48b32b6fc2cbfd76feaa1f04c
Lior <Shilon199707@gmail.com>
2023-08-31 21:01:49 +0200

    (ADD) password recovery page

---
+++ src/components/PassRecoveryPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+/**
+ * Password recovery page component.
+ * @return {React.Component} - The Login page component.
+ */
+function PassRecoveryPage() {
+    return (
+        <div>PassRecoveryPage</div>
+    );
+}
+
+export default PassRecoveryPage;
