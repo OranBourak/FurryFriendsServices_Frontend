30353c2dbc882139e5239ade611e95d45b663665
Lior <Shilon199707@gmail.com>
2023-09-04 17:45:25 +0200

    (ADD) clendar handles blocked dates, blocked hours and hours for booked appointments

--- src/components/MeetingCalendar.jsx
+++ src/components/MeetingCalendar.jsx
@@ -1,10 +1,50 @@
 /* eslint-disable no-unused-vars */
 import {useState, React} from "react";
+import {format, isSameDay} from "date-fns";
 import "react-calendar/dist/Calendar.css";
 import {Calendar as MyCalendar} from "react-calendar";
 import Time from "./Time";
 import "./MeetingCalendar.css";
-import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
+
+const serviceProvider = {
+    country: "",
+    city: "",
+    name: "",
+    gender: ["Male", "Female", "Other"],
+    picture: "",
+    email: "",
+    phone: "",
+    bio: "",
+    password: "",
+    typeOfService: "",
+    reviews: [],
+    averageRating: "",
+    blockedDates: ["Sep 7 2023"],
+    blockedTimeSlots: [
+        {
+            date: "Sep 8 2023",
+            blockedHours: ["08:00", "10:00"],
+        },
+    ],
+    appointments: [
+        {
+            id: 2,
+            clientName: "Jane Smith",
+            phoneNumber: "987-654-3210",
+            appointmentType: "Follow-up",
+            date: new Date(2023, 8, 15, 14, 0), // Date format: Year, Month (0-based index), Day, Hour, Minute
+        },
+        {
+            id: 3,
+            clientName: "Jane For",
+            phoneNumber: "987-654-3210",
+            appointmentType: "Follow-up",
+            date: new Date(2023, 9, 15, 14, 0), // Date format: Year, Month (0-based index), Day, Hour, Minute
+        },
+    ],
+    appointmentTypes: [],
+};
 
 /**
  * Component to display a calendar with date selection and appointment times.
@@ -13,74 +53,72 @@ import PropTypes from "prop-types";
 const MeetingCalendar = () => {
     const [date, setDate] = useState(new Date());
     const [showTime, setShowTime] = useState(false);
-    const [selectedTimes, setSelectedTimes] = useState([]);
+    const [isDayBlocked, setIsDayBlocked] = useState(false);
+    const [blockedHours, setBlockedHours] = useState([]);
+    const [currDateAppointments, setCurrDateAppointments] = useState([]);
 
-    const getChosenHours = (hoursArr) => {
-        setSelectedTimes(hoursArr);
-        console.log("hour outside : " + hoursArr);
+    const filterAppointemtnsByDay = (date, appointmentsArr) => {
+        return appointmentsArr.filter((appointemt)=>{
+            return isSameDay(appointemt.date, date);
+        });
     };
 
     /**  changes the date eselected in the calendar and shows the available hours
-    * @param {Event} e
-    */
+     * @param {Event} e
+     */
     function onClickDate(e) {
-        setShowTime(true);
-        const year = e.getFullYear(); // Get the year (e.g., 2023)
-        const month = e.getMonth(); // Get the month (0 for January, 11 for December)
-        const day = e.getDate(); // Get the day of the month (e.g., 16)
-        const hours = e.getHours(); // Get the hours (e.g., 0)
-        const minutes = e.getMinutes(); // Get the minutes (e.g., 0)
-        const seconds = e.getSeconds(); // Get the seconds (e.g., 0)
-        const milliseconds = e.getMilliseconds(); // Get the milliseconds (e.g., 0)
-
-        console.log("Year:", year);
-        console.log("Month:", month); // Note: Months are zero-based, so September is 8 (0-based).
-        console.log("Day:", day);
-        console.log("Hours:", hours);
-        console.log("Minutes:", minutes);
-        console.log("Seconds:", seconds);
-        console.log("Milliseconds:", milliseconds);
-        getDate(e);
-    }
-    /** prints the date in the format of: month day year
-    * @param {Date} dateObj
-    */
-    function getDate(dateObj) {
-        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        const month = monthNames[dateObj.getMonth()]; // Get the month name
-
-        // Get the day and year
-        const day = dateObj.getDate();
-        const year = dateObj.getFullYear();
-
-        // Create the formatted string
-        const formattedDate = `${month} ${day} ${year}`;
-        console.log("formated date: " + formattedDate);
-    }
-    /** Handles date change in the calendar
-    * @param {Event} e
-    */
-    function onSetDate(e) {
         setDate(e);
+        setShowTime(true);
+
+        const newDate = e;
+        const formatString = "MMM d yyyy";
+        const newDateToString = format(newDate, formatString);
+        const blockedDates = serviceProvider.blockedDates;
+        const blockedTimeSlots = serviceProvider.blockedTimeSlots;
+        const chosenDateAppointments = filterAppointemtnsByDay(newDate, serviceProvider.appointments);
+        // If the chosen date is a blocked date
+        if (blockedDates.includes(newDateToString)) {
+            setIsDayBlocked(true);
+        } else {
+            // If it's not a blocked date
+            setIsDayBlocked(false);
+            // Check if the chosen date has blocked hours
+            blockedTimeSlots.forEach((timeslot) => {
+                // If the chosen date appears in the blockedTimeSlots
+                if (timeslot.date === newDateToString) {
+                    // set the date's blocked timeslots to the blocked hours list.
+                    setBlockedHours(timeslot.blockedHours);
+                } else {
+                    setBlockedHours([]);
+                }
+                // Check if the chosen date has booked appointmens
+                if (chosenDateAppointments.length > 0) {
+                    setCurrDateAppointments(chosenDateAppointments);
+                } else {
+                    setCurrDateAppointments([]);
+                }
+            });
+        }
     }
+    // /** Handles date change in the calendar
+    //  * @param {Event} e
+    //  */
+    // function onSetDate(e) {
+    // setDate(e);
+    // }
 
     return (
         <div className="calendar-page">
             <h1>React Calendar</h1>
             <div className="calendar-container">
-                <MyCalendar
-                    onChange={onSetDate}
-                    value={date}
-                    defaultView="month"
-                    onClickDay={onClickDate}
-                />
+                <MyCalendar value={date} defaultView="month" onClickDay={onClickDate} />
             </div>
-            <Time showTime={showTime} date={date} setHour={getChosenHours} />
+            <Button variant="secondary">Block Day</Button>
+            {" "}
+            <Button variant="dark">Block Hours</Button>
+            <Time showTime={showTime} date={date} isDayBlocked={isDayBlocked} blockedHours={blockedHours} scheduledAppointments={currDateAppointments} />
         </div>
     );
-};
-
-MeetingCalendar.propTypes = {
 };
 
 export default MeetingCalendar;
