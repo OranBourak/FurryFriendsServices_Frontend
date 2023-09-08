4cf68c682167023f9fff4cdc62d5e948e9e21098
Lior <Shilon199707@gmail.com>
2023-09-08 03:01:04 +0200

    (ADD) radio button for selecting the type of user. Sends the user infi to the backend

--- src/pages/ServiceProviderPages/LoginPage.jsx
+++ src/pages/ServiceProviderPages/LoginPage.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
 import React, {useState} from "react";
 import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
@@ -6,6 +7,7 @@ import Form from "react-bootstrap/Form";
 import axios from "axios";
 import "../../styles/ServiceProviderStyles/LoginPage.css";
 import {useNavigate} from "react-router-dom";
+import {ButtonGroup, ToggleButton} from "react-bootstrap";
 
 
 /**
@@ -14,6 +16,7 @@ import {useNavigate} from "react-router-dom";
  * @return {React.Component} - The Login page component.
  */
 function LoginPage({onLogin}) {
+    const [userType, setUserType] = useState(""); // Default to 'Client'
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
@@ -26,7 +29,8 @@ function LoginPage({onLogin}) {
      */
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if (!email.trim() || !password.trim()) {
+        console.log(userType);
+        if (!email.trim() || !password.trim() || !userType.trim()) {
             setError("Please fill in all fields.");
             return;
         }
@@ -34,10 +38,13 @@ function LoginPage({onLogin}) {
         // for commit: https://furryfriendsbackend.onrender.com/login
         // for testing: http://localhost:5000/login
         try {
-            const response = await axios.post("http://localhost:5000/login", {
+            const response = await axios.post(`/${userType}/login`, {
+                user: userType,
                 email: email,
                 password: password,
             });
+            // TODO: handle the token
+            console.log("In login page, user exists");
             const data = response.data;
             handleLogIn(data.name);
             console.log("name: " + data.name);
@@ -80,7 +87,11 @@ function LoginPage({onLogin}) {
         navigate("/pass-recovery");
     };
 
-    const isFormIncomplete = !email.trim() || !password.trim(); // Check if either field is empty
+    const isFormIncomplete = !email.trim() || !password.trim() || !userType.trim(); // Check if either field is empty
+    const radios = [
+        {name: "Client", value: "client"},
+        {name: "Service Provider", value: "serviceProvider"},
+    ];
 
     return (
         <div className="login-page-container">
@@ -90,6 +101,28 @@ function LoginPage({onLogin}) {
                     padding: "20px",
                     borderRadius: "10px"}}>
                     {error && <div className="text-danger mb-3">{error}</div>}
+                    {/* user type */}
+                    <Form.Group className="mb-3" controlId="formBasicUserType">
+                        <Form.Label>User Type</Form.Label>
+                        <div>
+                            <ButtonGroup className="mb-2">
+                                {radios.map((radio, idx) => (
+                                    <ToggleButton
+                                        key={idx}
+                                        id={`radio-${idx}`}
+                                        type="radio"
+                                        variant="secondary"
+                                        name="radio"
+                                        value={radio.value}
+                                        checked={userType === radio.value}
+                                        onChange={(e) => setUserType(e.currentTarget.value)}
+                                    >
+                                        {radio.name}
+                                    </ToggleButton>
+                                ))}
+                            </ButtonGroup>
+                        </div>
+                    </Form.Group>
                     {/* email input */}
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
