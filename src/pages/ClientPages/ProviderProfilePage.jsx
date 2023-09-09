691e1c7d5eec23dc9f181e7e84fce1b84d5e5a06
Oran Bourak <oranbourak@gmail.com>
2023-09-09 15:20:13 +0300

    Review component added to the SP page

--- src/pages/ClientPages/ProviderProfilePage.jsx
+++ src/pages/ClientPages/ProviderProfilePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ServiceProviderInfo from "../../components/client_components/ServiceProviderInfo.jsx";
+import ReviewCarousel from "../../components/client_components/ServiceProviderReviews.jsx";
 import {Row, Col, Card, Calendar, TimePicker, Button} from "antd";
 import moment from "moment";
 
@@ -15,11 +16,7 @@ const ProviderProfilePage = ({providerID}) => {
                     <ServiceProviderInfo providerID={proID} />
 
                     {/* Reviews */}
-                    <Card title="Reviews">
-                        <p>Review 1: Great service!</p>
-                        <p>Review 2: Very professional.</p>
-                        <p>Review 3: Could be better.</p>
-                    </Card>
+                    <ReviewCarousel providerID={proID} />
                 </Col>
 
                 {/* Second Column */}
@@ -43,20 +40,6 @@ const ProviderProfilePage = ({providerID}) => {
 
 /* ####  DELETE BELOW INFO ! JUST FOR TESTING  #####*/
 const proID = "64f9fc4b8c7d7c3657dc5b7b";
-
-//   const serviceProvider = {
-//     // Your service provider data here, fetched from your database
-//     name: 'John Doe',
-//     email: 'john.doe@example.com',
-//     phone: '1234567890',
-//     country: 'Israel',
-//     city: 'Tel Aviv',
-//     gender: 'Male',
-//     typeOfService: 'Dog Walker',
-//     averageRating: 4.5,
-//     bio: 'Experienced dog walker...',
-//     image: '../images/ServiceProvidersImages/default.jpg', // Replace with actual image path
-//   };
 
 
 export default ProviderProfilePage;
