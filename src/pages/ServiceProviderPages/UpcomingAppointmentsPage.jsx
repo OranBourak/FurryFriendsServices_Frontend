37a8a26d4ff96a7f3ec0a76656224639e3048557
VladFearo <69718459+VladFearo@users.noreply.github.com>
2023-09-10 21:41:53 +0300

    added logic to get appointments from database

--- src/pages/ServiceProviderPages/UpcomingAppointmentsPage.jsx
+++ src/pages/ServiceProviderPages/UpcomingAppointmentsPage.jsx
@@ -2,80 +2,113 @@ import React, {useEffect, useState} from "react";
 import {Table, Alert} from "react-bootstrap";
 import "../../styles/ServiceProviderStyles/upcomingAps.css";
 import moment from "moment"; // Import moment.js
+import {useAuth} from "../../context/AuthContext.js";
+import axios from "axios";
+import {Navigate} from "react-router-dom";
 
-const initialAppointments = [
-    {
-        id: 1,
-        clientName: "John Doe",
-        phoneNumber: "123-456-7890",
-        appointmentType: "Consultation",
-        date: "2023-09-10",
-        time: "09:00 AM",
-    },
-    {
-        id: 2,
-        clientName: "Jane Smith",
-        phoneNumber: "987-654-3210",
-        appointmentType: "Follow-up",
-        date: "2023-09-15",
-        time: "02:00 PM",
-    },
-    {
-        id: 3,
-        clientName: "Alice Johnson",
-        phoneNumber: "555-123-4567",
-        appointmentType: "Check-up",
-        date: "2023-09-20",
-        time: "10:00 AM",
-    },
-    {
-        id: 4,
-        clientName: "Bob Brown",
-        phoneNumber: "555-987-6543",
-        appointmentType: "Consultation",
-        date: "2023-09-25",
-        time: "03:00 PM",
-    },
-    {
-        id: 5,
-        clientName: "John Doe",
-        phoneNumber: "123-456-7890",
-        appointmentType: "Consultation",
-        date: "2023-09-10",
-        time: "10:00 AM",
-    },
-    {
-        id: 6,
-        clientName: "John Doe",
-        phoneNumber: "123-456-7890",
-        appointmentType: "Consultation",
-        date: "2023-08-10",
-        time: "10:00 AM",
-    },
-    // Add more appointments here...
-];
+// const initialAppointments = [
+//     {
+//         id: 1,
+//         clientName: "John Doe",
+//         phoneNumber: "123-456-7890",
+//         appointmentType: "Consultation",
+//         date: "2023-09-10",
+//         time: "09:00 AM",
+//     },
+//     {
+//         id: 2,
+//         clientName: "Jane Smith",
+//         phoneNumber: "987-654-3210",
+//         appointmentType: "Follow-up",
+//         date: "2023-09-15",
+//         time: "02:00 PM",
+//     },
+//     {
+//         id: 3,
+//         clientName: "Alice Johnson",
+//         phoneNumber: "555-123-4567",
+//         appointmentType: "Check-up",
+//         date: "2023-09-20",
+//         time: "10:00 AM",
+//     },
+//     {
+//         id: 4,
+//         clientName: "Bob Brown",
+//         phoneNumber: "555-987-6543",
+//         appointmentType: "Consultation",
+//         date: "2023-09-25",
+//         time: "03:00 PM",
+//     },
+//     {
+//         id: 5,
+//         clientName: "John Doe",
+//         phoneNumber: "123-456-7890",
+//         appointmentType: "Consultation",
+//         date: "2023-09-10",
+//         time: "10:00 AM",
+//     },
+//     {
+//         id: 6,
+//         clientName: "John Doe",
+//         phoneNumber: "123-456-7890",
+//         appointmentType: "Consultation",
+//         date: "2023-08-10",
+//         time: "10:00 AM",
+//     },
+//     // Add more appointments here...
+// ];
 
 const UpcomingAppointments = () => {
-    const [appointments, setAppointments] = useState(initialAppointments);
+    const {loggedIn, userData} = useAuth();
+    const [appointments, setAppointments] = useState();
 
-    useEffect(() => {
+    const getAppointments = async () => {
+        if (loggedIn) {
+            try {
+                const response = await axios.get(`/serviceProvider/get/${userData.id}`, {
+                    headers: {
+                        Authorization: `Bearer ${userData.token}`, // Replace 'userToken' with the actual user token
+                    },
+                });
+                const {appointments} = response.data;
+                setAppointments(appointments.populate());
+            } catch (error) {
+                console.error(error);
+            };
+        }
+    };
+    useEffect(async () => {
+        try {
+            await getAppointments();
+        } catch (error) {
+            console.error(error);
+        }
+        if (appointments) {
         // Filter appointments that are on or after today
-        const today = moment();
-        const filteredAppointments = appointments.filter((appointment) => {
-            const appointmentDate = moment(`${appointment.date} ${appointment.time}`, "YYYY-MM-DD hh:mm A");
-            return appointmentDate.isSameOrAfter(today, "day"); // Check if appointment is on or after today
-        });
+            const today = moment();
+            const filteredAppointments = appointments.filter((appointment) => {
+                const appointmentDate = moment(`${appointment.date} ${appointment.time}`, "YYYY-MM-DD hh:mm A");
+                return appointmentDate.isSameOrAfter(today, "day"); // Check if appointment is on or after today
+            });
 
-        // Sort the filtered appointments based on a combined timestamp (date + time)
-        const sortedAppointments = filteredAppointments.sort((a, b) => {
-            const timestampA = moment(`${a.date} ${a.time}`, "YYYY-MM-DD hh:mm A").valueOf();
-            const timestampB = moment(`${b.date} ${b.time}`, "YYYY-MM-DD hh:mm A").valueOf();
+            // Sort the filtered appointments based on a combined timestamp (date + time)
+            const sortedAppointments = filteredAppointments.sort((a, b) => {
+                const timestampA = moment(`${a.date} ${a.time}`, "YYYY-MM-DD hh:mm A").valueOf();
+                const timestampB = moment(`${b.date} ${b.time}`, "YYYY-MM-DD hh:mm A").valueOf();
 
-            return timestampA - timestampB;
-        });
-
-        setAppointments(sortedAppointments);
+                return timestampA - timestampB;
+            });
+            setAppointments(sortedAppointments);
+        }
     }, []); // This effect runs only once on component mount
+
+
+    if (!loggedIn) {
+        // Redirect to the login page or another protected route
+        return <Navigate to="/login" />;
+    } else if (userData.userType !== "serviceProvider") {
+        return <Navigate to="/error"/>;
+    }
 
     return (
         <>
