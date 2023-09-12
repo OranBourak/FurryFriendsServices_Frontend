1b7e98521ab445965ba0851f72113a6d79bfbe79
Lior <Shilon199707@gmail.com>
2023-09-12 06:10:49 +0300

    (ADD) Add proptype: setIsAfterBlockOperation(function) - used to set a flag if in a block operation

--- src/components/ServiceProviderComponents/Time.jsx
+++ src/components/ServiceProviderComponents/Time.jsx
@@ -10,13 +10,14 @@ import "../../styles/ServiceProviderStyles/MeetingCalendar.css";
  * @param {Date} props.date - The Date object to display the time from.
  * @param {boolean} props.showTime - A flag to determine whether to show the time or not.
  * @param {function} props.setHour - Sets the hour in the calendar component
+ * @param {function} props.setIsAfterBlockOperation - re-renders the page after block operation
  * @return {React.Component} - The component displaying the time or null if 'showTime' is false.
  */
 function Time(props) {
     return (
         <div className="appointment-time">
             {props.showTime ? (
-                <Times date={props.date} isDayBlocked={props.isDayBlocked} blockedHours={props.blockedHours} scheduledAppointments={props.scheduledAppointments} />
+                <Times date={props.date} isDayBlocked={props.isDayBlocked} blockedHours={props.blockedHours} scheduledAppointments={props.scheduledAppointments} setIsAfterBlockOperation={props.setIsAfterBlockOperation} />
             ) : null}
         </div>
     );
@@ -28,6 +29,8 @@ Time.propTypes = {
     isDayBlocked: PropTypes.bool,
     blockedHours: PropTypes.arrayOf(PropTypes.string),
     scheduledAppointments: PropTypes.arrayOf(PropTypes.object),
+    setHour: PropTypes.func,
+    setIsAfterBlockOperation: PropTypes.func,
 };
 
 export default Time;
