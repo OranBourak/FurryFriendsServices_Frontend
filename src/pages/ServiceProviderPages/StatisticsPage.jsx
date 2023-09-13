68fba63ccaf75d33732e4815f2efdb35087627cb
Lior <Shilon199707@gmail.com>
2023-09-13 11:29:40 +0300

    (ADD) Getting the service provider's past 5 months appointments and his appointment types from the backend

--- src/pages/ServiceProviderPages/StatisticsPage.jsx
+++ src/pages/ServiceProviderPages/StatisticsPage.jsx
@@ -15,12 +15,28 @@ function StatisticsPage() {
         if (loggedIn) {
             console.log("user is logged in");
             try {
-                // setIsLoading(true);
-                await axios.get(`/appointment/getPast5MonthsAppointments/${userData.id}`, {
+                // Get the service provider's appoitnemtns of the past 5 months
+                const appResponse = await axios.get(`/appointment/getPast5MonthsAppointments/${userData.id}`, {
                     headers: {
                         Authorization: `Bearer ${userData.token}`, // Replace 'userToken' with the actual user token
                     },
                 });
+
+                const appointments = appResponse.data.appointments;
+                console.log("app 5 months ago:" + appointments);
+                console.log(appointments[0].date);
+                console.log(appointments[0].appointmentType[0].name);
+
+                // Get the appointment types of the service provider
+                const appTypeResponse = await axios.get(`/serviceProvider/getAppointmentTypes/${userData.id}`, {
+                    headers: {
+                        Authorization: `Bearer ${userData.token}`, // Replace 'userToken' with the actual user token
+                    },
+                });
+                const appointmentTypes = appTypeResponse.data.appointmentTypes;
+                console.log("app types: "+ appointmentTypes);
+                // Create a function for getting the index of an appointment type in the appointmentTypes objects array
+                // For each appointment, find the index of its appointment type and add to this index her price
             } catch (error) {
                 console.log(error);
                 message.error({
