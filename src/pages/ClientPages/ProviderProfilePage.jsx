7c408cce1cc38e7f50e966c534352c8892091c82
Oran Bourak <oranbourak@gmail.com>
2023-09-15 16:42:35 +0300

    Login and service provider pages styling added

--- src/pages/ClientPages/ProviderProfilePage.jsx
+++ src/pages/ClientPages/ProviderProfilePage.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import {React, useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import ServiceProviderInfo from "../../components/client_components/ServiceProviderInfo.jsx";
 import ReviewCarousel from "../../components/client_components/ServiceProviderReviews.jsx";
 import ScheduleAppointment from "../../components/client_components/scheduleAppointment.jsx";
 import "../../styles/ClientStyles/ProviderProfilePage.css";
-import {Row, Col} from "antd";
+import {Row, Col, Skeleton} from "antd";
 
 const ProviderProfilePage = () => {
     const {id} = useParams();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulate an API call to fetch data
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000); // 1 seconds delay
+    }, []);
+
     return (
         <div className="mainPage">
             <h1 className="mainPageTitle">Profile</h1>
@@ -16,12 +25,16 @@ const ProviderProfilePage = () => {
                 <Col span={8} className="Col">
                     {/* Service Provider Info */}
                     <div className="ServiceProviderInfo">
-                        <ServiceProviderInfo providerID={id} />
+                        {loading ? (
+                            <Skeleton active />
+                        ) : (
+                            <ServiceProviderInfo providerID={id} />
+                        )}
                     </div>
 
                     {/* Reviews */}
                     <div className="ReviewCarousel">
-                        <ReviewCarousel providerID={id} />
+                        {loading ? <Skeleton active /> : <ReviewCarousel providerID={id} />}
                     </div>
                 </Col>
 
@@ -29,7 +42,11 @@ const ProviderProfilePage = () => {
                 <Col span={8} className="Col">
                     {/* Schedule Meeting */}
                     <div className="ScheduleAppointment">
-                        <ScheduleAppointment providerID={id}/>
+                        {loading ? (
+                            <Skeleton active />
+                        ) : (
+                            <ScheduleAppointment providerID={id} />
+                        )}
                     </div>
                 </Col>
             </Row>
