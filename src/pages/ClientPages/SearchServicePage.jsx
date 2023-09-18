92e2a2879aa3cdad00d57c76ece6c5a61049342c
TomerBurman <93316511+TomerBurman@users.noreply.github.com>
2023-09-19 00:29:25 +0200

    Fixed deleting issue

--- src/pages/ClientPages/SearchServicePage.jsx
+++ src/pages/ClientPages/SearchServicePage.jsx
@@ -4,7 +4,7 @@ import {Typography} from "antd";
 import SearchForm from "../../components/client_components/SearchForServiceForm.jsx";
 import ServiceProviderResults from "../../components/client_components/SearchServiceResults.jsx";
 import "../../styles/ClientStyles/SearchServicePage.css";
-import {message} from "antd";
+import {message, Skeleton, Steps} from "antd";
 
 
 const {Text} = Typography;
@@ -17,6 +17,8 @@ const {Text} = Typography;
 function SearchServicePage() {
     // State to hold the service provider data
     const [serviceData, serviceDataSet] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const description = "Please select a service provider";
 
     /**
      * handleSearch is an asynchronous function that fetches service provider data based on search parameters.
@@ -25,6 +27,7 @@ function SearchServicePage() {
      * @param {Object} event - The search parameters
      */
     const handleSearch = async (event) => {
+        setIsLoading(true); // Set loading to true when search starts
         try {
             // Make an API call to search for service providers based on the search parameters
             const response = await axios.get("/client/searchProviders/", {
@@ -46,22 +49,45 @@ function SearchServicePage() {
                 style: {yIndex: 1000, fontSize: "24px"},
 
             }, 2);
+        } finally {
+            setIsLoading(false); // Set loading to false when search is complete
         }
     };
 
     return (
         // Main container for the search page
         <div className="search-page-container">
+            <Steps
+                current={0}
+                status="process"
+                items={[
+                    {
+                        title: "Selecting a service provider",
+                        description,
+                    },
+                    {
+                        title: "Select Appointment type and time",
+                    },
+                    {
+                        title: "Appointment has been scheduled",
+                    },
+                ]}
+            />
             {/* Container for the search form */}
             <div className="search-form-container">
                 {/* SearchForm component to handle the search input */}
-                <SearchForm handleSearch={handleSearch} />
+                <SearchForm handleSearch={handleSearch} isLoading={isLoading} />
             </div>
             {/* Container for displaying the search results */}
             <div className="service-provider-results-container">
-                {/* ServiceProviderResults component to display the list of service providers */}
-                <ServiceProviderResults serviceData={serviceData} />
-                <Text style={{fontSize: "20px"}}>#Results: {serviceData.length}</Text>
+                {isLoading ? (
+                    <Skeleton active /> // Show loading skeleton if isLoading is true
+                ) : (
+                    <> {/* ServiceProviderResults component to display the list of service providers */}
+                        <ServiceProviderResults serviceData={serviceData} />
+                        <Text style={{fontSize: "20px"}}>#Results: {serviceData.length}</Text>
+                    </>
+                )}
             </div>
         </div>
     );
